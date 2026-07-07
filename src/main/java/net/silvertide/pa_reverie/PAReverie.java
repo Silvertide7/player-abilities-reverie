56704decf93ff920e2f4334adbb16b2318bdc985
Silvertide7 <natephillips801@gmail.com>
2026-07-06 18:47:23 -0600

    initial project setup

---
+++ src/main/java/net/silvertide/pa_reverie/PAReverie.java
@@ -0,0 +1,8 @@
+package net.silvertide.pa_reverie;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(PAReverie.MOD_ID)
+public class PAReverie {
+    public static final String MOD_ID = "pa_reverie";
+}
