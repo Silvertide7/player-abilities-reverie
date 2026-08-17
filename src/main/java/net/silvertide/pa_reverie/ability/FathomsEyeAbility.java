029d8c855a22edc8805bdc7dd3213300575f5293
Silvertide7 <natephillips801@gmail.com>
2026-08-16 20:50:41 -0600

    1.0.5 - Fix sight abilities

--- src/main/java/net/silvertide/pa_reverie/ability/FathomsEyeAbility.java
+++ src/main/java/net/silvertide/pa_reverie/ability/FathomsEyeAbility.java
@@ -8,7 +8,7 @@ import net.silvertide.pa_reverie.registry.ReverieEffects;
 import net.silvertide.player_abilities.api.AbilityUseType;
 
 public final class FathomsEyeAbility extends HarvestAbility {
-    private static final int[] COOLDOWN_SECONDS_BY_LEVEL = {300, 480, 900};
+    private static final int COOLDOWN_SECONDS = 300;
 
     @Override
     public AbilityUseType getUseType() {
@@ -22,7 +22,7 @@ public final class FathomsEyeAbility extends HarvestAbility {
 
     @Override
     public int getCooldownTicks(int level) {
-        return byLevel(level, COOLDOWN_SECONDS_BY_LEVEL) * TICKS_PER_SECOND;
+        return COOLDOWN_SECONDS * TICKS_PER_SECOND;
     }
 
     @Override
@@ -39,7 +39,7 @@ public final class FathomsEyeAbility extends HarvestAbility {
     public void onUseReleased(ServerPlayer player, int level) {
         if (player.isEyeInFluid(FluidTags.WATER)) {
             player.addEffect(new MobEffectInstance(ReverieEffects.FATHOMS_EYE,
-                    byLevel(level, 3600, 7200, 18000), level - 1, false, false, true));
+                    byLevel(level, 6000, 9600, 18000), level - 1, false, false, true));
         }
     }
 }
